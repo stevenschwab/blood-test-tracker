081706460c7e1b06f234e404f3a4aecc8c99228a
Steven Schwab <schwabs@umich.edu>
2025-04-23 16:45:47 -0400

    add authform and userlist

---
+++ client/src/components/AuthForm.js
@@ -0,0 +1,54 @@
+// The form for registration and login.
+import React, { useState } from 'react'
+
+function AuthForm({ onResponse, onError, token }) {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = (action) => (e) => {
+        e.preventDefault()
+        const credentials = { username, password }
+
+        fetch(`/api/auth/${action}`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(credentials)
+        })
+            .then(res => res.json())
+            .then(data => {
+                onResponse(data)
+                if (data.token) {
+                    setUsername('')
+                    setPassword('')
+                }
+            })
+            .catch(onError)
+    }
+
+    return (
+        <form>
+            <div>
+                <input
+                    type='type'
+                    placeholder='Username'
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+            </div>
+            <div>
+                <input
+                    type='password'
+                    placeholder='Password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </div>
+            <div>
+                <button onClick={handleSubmit('register')} disabled={!!token}>Register</button>
+                <button onClick={handleSubmit('login')}>Log in</button>
+            </div>
+        </form>
+    )
+}
+
+export default AuthForm
---
+++ client/src/components/UserList.js
@@ -0,0 +1,14 @@
+// The component to display the list of users
+import React from 'react'
+
+function UserList({ users }) {
+    return (
+        <div id='userList'>
+            {users.map((user, index) => (
+                <div key={index}>{user.username}</div>
+            ))}
+        </div>
+    )
+}
+
+export default UserList
