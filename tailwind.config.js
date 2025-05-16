b334973aafd8f134ff54a99512cb3f32bd12719a
Steven Schwab <schwabs@umich.edu>
2025-05-16 14:59:20 -0400

    edit tailwind config to include react component files

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./client/src/**/*.{js,jsx,ts,tsx}"
+  ], // Where the components are
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
