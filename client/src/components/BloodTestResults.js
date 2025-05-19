0557792a8384cb449bf6d05c8db5522917f6648c
Steven Schwab <schwabs@umich.edu>
2025-05-19 11:47:41 -0400

    removed white space

--- client/src/components/BloodTestResults.js
+++ client/src/components/BloodTestResults.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 import './BloodResultsTable.css';
