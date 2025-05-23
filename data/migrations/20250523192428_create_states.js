6fa6fd42c7f348388ca06ec18e3e47c367d888d2
Steven Schwab <schwabs@umich.edu>
2025-05-23 15:30:26 -0400

    Add states migration and seed

---
+++ data/migrations/20250523192428_create_states.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('states', states => {
+        states.increments('state_id').primary()
+        states.string('state_name', 50).notNullable().unique()
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('states');
+};
---
+++ data/seeds/states.js
@@ -0,0 +1,60 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+exports.seed = async function(knex) {
+  // Deletes ALL existing entries
+  await knex('states').del()
+  await knex('states').insert([
+    { state_name: 'Alabama' },
+    { state_name: 'Alaska' },
+    { state_name: 'Arizona' },
+    { state_name: 'Arkansas' },
+    { state_name: 'California' },
+    { state_name: 'Colorado' },
+    { state_name: 'Connecticut' },
+    { state_name: 'Delaware' },
+    { state_name: 'Florida' },
+    { state_name: 'Georgia' },
+    { state_name: 'Hawaii' },
+    { state_name: 'Idaho' },
+    { state_name: 'Illinois' },
+    { state_name: 'Indiana' },
+    { state_name: 'Iowa' },
+    { state_name: 'Kansas' },
+    { state_name: 'Kentucky' },
+    { state_name: 'Louisiana' },
+    { state_name: 'Maine' },
+    { state_name: 'Maryland' },
+    { state_name: 'Massachusetts' },
+    { state_name: 'Michigan' },
+    { state_name: 'Minnesota' },
+    { state_name: 'Mississippi' },
+    { state_name: 'Missouri' },
+    { state_name: 'Montana' },
+    { state_name: 'Nebraska' },
+    { state_name: 'Nevada' },
+    { state_name: 'New Hampshire' },
+    { state_name: 'New Jersey' },
+    { state_name: 'New Mexico' },
+    { state_name: 'New York' },
+    { state_name: 'North Carolina' },
+    { state_name: 'North Dakota' },
+    { state_name: 'Ohio' },
+    { state_name: 'Oklahoma' },
+    { state_name: 'Oregon' },
+    { state_name: 'Pennsylvania' },
+    { state_name: 'Rhode Island' },
+    { state_name: 'South Carolina' },
+    { state_name: 'South Dakota' },
+    { state_name: 'Tennessee' },
+    { state_name: 'Texas' },
+    { state_name: 'Utah' },
+    { state_name: 'Vermont' },
+    { state_name: 'Virginia' },
+    { state_name: 'Washington' },
+    { state_name: 'West Virginia' },
+    { state_name: 'Wisconsin' },
+    { state_name: 'Wyoming' }
+  ]);
+};
