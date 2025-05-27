76d0bf7b6fba5affa494b096033c3c874180bebf
Steven Schwab <schwabs@umich.edu>
2025-05-27 16:42:29 -0400

    add email field to users table

---
+++ data/migrations/20250527203626_add_email_to_users.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.table('users', users => {
+    users.string('email').unique().notNullable().defaultTo('');
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.table('users', users => {
+    users.dropColumn('email');
+  })
+};
